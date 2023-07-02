6ecdb13167fc3e024f4adf915da471973e0a5d3d
TallTall22 <syuns1205@gmail.com>
2023-07-02 16:45:43 +0800

    新增註解

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,11 +2,22 @@ const express=require('express')
 const router=express.Router()
 const adminController=require('../../controllers/admin-controller')
 
+//場域編輯頁面
 router.get('/sites/:id/edit',adminController.editSite)
+
+//場域詳細資料，包含溫室氣體、農作物、能源
 router.get('/sites/:id',adminController.getSite)
+
+//新增場域資料，包含溫室氣體、農作物、能源
 router.put('/sites/:id',adminController.putSite)
+
+//刪除場域資料，同場域的溫室氣體、農作物、能源會被刪掉
 router.delete('/sites/:id',adminController.deleteSite)
+
+//所有場域資料，只有場域
 router.get('/sites',adminController.getSites)
+
+//新增場域，包含溫室氣體、農作物、能源
 router.post('/sites',adminController.postSite)
 
 module.exports=router
